61cbfe1d7a17531e7fb6c5aa3ff1a7181174af85
Arman <armanurstem@gmail.com>
2024-05-26 16:33:11 +0500

    add WeatherManager

---
+++ WeatherApp/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  WeatherApp
+//
+//  Created by Arman  Urstem on 26.05.2024.
+//
+
+import Foundation
---
+++ WeatherApp/View/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Arman  Urstem on 26.05.2024.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
