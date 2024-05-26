f20f8b3e197fbdabf3138989574730e5033777d3
Arman <armanurstem@gmail.com>
2024-05-26 15:35:48 +0500

    Create Managers and WelcomeView

--- WeatherApp/View/ContentView.swift
+++ WeatherApp/View/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationManger = LocationManager()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
