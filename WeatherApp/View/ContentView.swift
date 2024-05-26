93b4aadb935e73e7a7d342a78e6f7430f65a1ffa
Arman <armanurstem@gmail.com>
2024-05-26 16:41:57 +0500

    changed option ContentView

--- WeatherApp/View/ContentView.swift
+++ WeatherApp/View/ContentView.swift
@@ -9,12 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     
     var body: some View {
         VStack {
             
             if let location = locationManager.location {
-                Text("Your location are:        \(location.longitude),      \(location.latitude)")
+                if let weather = weather {
+                    Text("Weather data fetched!")
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
             } else {
                 if locationManager.isLoading {
                     LoadingView()
@@ -24,12 +37,12 @@ struct ContentView: View {
                 }
             }
         }
-                    .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
-                    .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
-            }
-            
-        }
-        
-        #Preview {
-            ContentView()
-        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
